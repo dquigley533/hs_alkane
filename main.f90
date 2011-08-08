cb7b12c99498b8fae4933540a988a116b9333ec7
phseal <phseal>
2011-08-08 10:57:45 +0000

    Added signal handler for SIGTERM

--- main.f90
+++ main.f90
@@ -3,13 +3,16 @@
 !                       H  S    A  L  K  A  N  E                              !
 !=============================================================================!
 !                                                                             !
-! $Id: main.f90,v 1.5 2011/08/02 12:56:47 phseal Exp $
+! $Id: main.f90,v 1.6 2011/08/08 10:57:45 phseal Exp $
 !                                                                             !
 !-----------------------------------------------------------------------------!
 ! Simulation code to perform NPT simulations of hard-sphere chain alkanes.    !
 !-----------------------------------------------------------------------------!
 !                                                                             !
 ! $Log: main.f90,v $
+! Revision 1.6  2011/08/08 10:57:45  phseal
+! Added signal handler for SIGTERM
+!
 ! Revision 1.5  2011/08/02 12:56:47  phseal
 ! Added C bindings to all procedures which should be callable externally
 ! when compiled as a library.
@@ -31,7 +34,32 @@
 !
 !
 !=============================================================================!
+subroutine cleanexit()
+  !-------------------------------------------------------------------------!
+  ! Routine to handle clean shutdown of code in the event of SIGTERM being  !
+  ! sent to the process by PBS or Condor etc.                               !
+  !-------------------------------------------------------------------------!
+  ! D.Quigley August 2011                                                   !
+  !-------------------------------------------------------------------------!
+  use box
+  use alkane
+  implicit none
+  integer :: ibox
 
+  write(0,'("Received signal SIGTERM - shutting down")')
+
+  do ibox = 1,nboxes
+     close(25+ibox-1) ! close density.dat
+  end do
+
+  !-------------------------------------!
+  ! Release memory                      !
+  !-------------------------------------!
+  call alkane_destroy()
+  call box_destroy_link_cells()
+  call box_destroy()
+
+end subroutine cleanexit
 
 program hs_alkane
 
@@ -55,11 +83,14 @@ program hs_alkane
   integer(kind=it) :: safe 
 
   ! Loop counters / error flags
-  integer(kind=it) :: ibead,ichain,ierr,ibox
+  integer(kind=it) :: ibead,ichain,ierr,ibox,noverlaps
 
   ! Strings
   character(30) :: denfile
   character(3)  :: boxstring
+
+  ! external signal handling subroutine
+  external :: cleanexit
 
   !--------------------------!
   ! Set timer module going   !
@@ -88,6 +119,18 @@ program hs_alkane
   if (read_xmol) then
      call io_read_xmol()
      call mc_initialise()
+
+     ! Debug testing of alkane_get_external_overlaps
+!!$     do ibox = 1,nboxes
+!!$        do ichain = 1,nchains
+!!$           call alkane_get_external_overlaps(ichain,ibox,noverlaps)
+!!$           write(0,'("Chain ",I5," in box ",I5, " has ",I5," external overlaps.")')ichain,ibox,noverlaps
+!!$           call alkane_get_internal_overlaps(ichain,ibox,noverlaps)
+!!$           write(0,'("Chain ",I5," in box ",I5, " has ",I5," internal overlaps.")')ichain,ibox,noverlaps
+!!$        end do
+!!$     end do
+
+
   else
      call mc_initialise(grow_new_chains=.true.)
   end if
@@ -110,6 +153,11 @@ program hs_alkane
      end do
   end if
 
+  !------------------------------------!
+  ! Setup a signal handler for SIGTERM !   
+  ! which is usually signal no. 15.    !
+  !------------------------------------!
+  call signal(15,cleanexit)
 
   !------------------------------------!
   ! Time measurement for cycle rate    !
